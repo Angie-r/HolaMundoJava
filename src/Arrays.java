3a6a9431d57848cf736e92bc672098d802c7141f
Angela Romero <anjera27@gmail.com>
2022-08-04 11:06:30 -0600

    Como declarar Arrays

---
+++ src/Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays {
+    public static void main(String[] args) {
+        //Cuantas versiones de android existen
+        /*/Un arreglo puede ser funcional cuando se tiene un dato especifico
+        por ejemplo los dias de la semana, es dato no cambiara.  */
+        String[] androidVersiones = new String[17];
+        String days[] = new String[7];
+
+        String[][] ciudades = new String[4][2]; //4*2 = 8 elementos o espacios
+
+        int [][][] numeros = new int[2][2][2];
+        int [][][][] numeros4 = new int[2][2][2][2];
+
+    }
+}
